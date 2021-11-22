98f139214b848e93327d8a8d19d11d755b29ca22
AlberttoG2 <albertt0801.ag@gmail.com>
2021-11-22 09:19:33 -0600

    Ajuste Contrato 2

---
+++ src/autoPresta/client/src/app/views/procesos/contrato/generacion-contrato/generacion-contrato.component.ts
@@ -0,0 +1,72 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {FormGroup, Validators} from "@angular/forms";
+import {Agencias, Combo, Folios, Portafolios} from "../../../../core/models/data.interface";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {RestService} from "../../../../core/service/rest.service";
+import {GlobalService} from "../../../../core/service/global.service";
+import {Subscription} from "rxjs";
+
+@Component({
+  selector: 'app-generacion-contrato',
+  templateUrl: './generacion-contrato.component.html',
+  styleUrls: ['./generacion-contrato.component.sass']
+})
+export class GeneracionContratoComponent implements OnInit {
+  action: string;
+  formulario: FormGroup;
+  dialogTitle: string;
+  calificacionClientesCombo: Combo[];
+  folios: Folios;
+
+  constructor(
+    public dialogRef: MatDialogRef<GeneracionContratoComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any, private globalService: GlobalService,
+    public restService: RestService,
+  ) {
+  }
+
+  submit() {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  public confirmAdd(): void {
+    this.dialogRef.close(this.formulario.value);
+  }
+
+  ngOnInit(): void {
+    this.dialogTitle = 'Generar contrato';
+
+    this.restService.combo<Combo[]>({id: 'CalificacionCliente'}, 'comboAutoPresta').subscribe(r => this.calificacionClientesCombo = r)
+    this.formulario = this.restService.buildForm({
+      calificacionCliente: [this.data.data.calificacionCliente ? this.data.data.calificacionCliente : ''],
+      fechaContrato: [this.data.data.fechaContrato ? this.data.data.fechaContrato : ''],
+      contratoPrueba: [this.data.data.contratoPrueba ? this.data.data.contratoPrueba : false],
+      numeroContrato: [this.folios ? this.folios.folio : ''],
+      referenciaBancariaBBVA: [this.data.data.referenciaBancariaBBVA ? this.data.data.referenciaBancariaBBVA : ''],
+      montoTransferencia: [this.data.data.montoTransferencia ? this.data.data.montoTransferencia : ''],
+      descuentosRetenciones: [this.data.data.descuentosRetenciones ? this.data.data.descuentosRetenciones : 'N/A'],
+      fechaSolicitud: [this.data.data.fechaSolicitud ? this.data.data.fechaSolicitud : ''],
+      montoLiquidar: [this.data.data.montoLiquidar ? this.data.data.montoLiquidar : ''],
+      fechaCompromiso: [this.data.data.fechaCompromiso ? this.data.data.fechaCompromiso : ''],
+    });
+    this.restService.edit<Portafolios>(1, 'Portafolios').subscribe(result => {
+      this.formulario.patchValue({fechaContrato: result.fecha + 'T00:00:00'});
+    });
+
+    this.restService.index<Folios>('Contrato', {}, 'folios').subscribe(r => {
+      this.folios = r
+      this.formulario.patchValue({numeroContrato: this.folios.folio})
+    })
+  }
+
+  cambiarFolio(checked: boolean) {
+    if (checked){
+      this.formulario.patchValue({numeroContrato: this.folios.folioPrueba})
+    } else {
+      this.formulario.patchValue({numeroContrato: this.folios.folio})
+    }
+  }
+}
